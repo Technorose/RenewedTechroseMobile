5b629cb64b85b752a0688a2d3d9df0428b939c47
Mehmet Solak <solakmehmet02@gmail.com>
2024-02-20 23:10:06 +0300

    warning disabled

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import { Provider } from "react-redux";
 import AppNavigation from "./navigations/AppNavigation";
 import Toast from 'react-native-toast-message';
 import { store } from "./store";
+import { LogBox } from "react-native";
 
 export default function App() {
+  LogBox.ignoreAllLogs([/Warning: .../]); // Ignore all log notifications
+
   return (
     <>
       <Provider store={store}>
